7f52e6d1abc80c5001c157609132b8f66812781e
Mikołaj Ozimek <mikolajozimek2@gmail.com>
2024-05-09 23:12:08 +0200

    Update Car_Insurance_Claim-Dataset-Analysis-with-Supervised-and-Unsupervised-learning-methods..R

--- Car_Insurance_Claim-Dataset-Analysis-with-Supervised-and-Unsupervised-learning-methods..R
+++ Car_Insurance_Claim-Dataset-Analysis-with-Supervised-and-Unsupervised-learning-methods..R
@@ -1,5 +1,5 @@
 
-# Pakiety -----------------------------------------------------------------
+# Packages -----------------------------------------------------------------
 library(Matrix)
 library(xgboost)
 library(e1071)
@@ -15,7 +15,7 @@ library(rattle)
 library(caret)
 library(ggplot2)
 library(pROC)
-# Poprawa danych ----------------------------------------------------------
+# Data correction ----------------------------------------------------------
 
 dane <- read.csv("Car_Insurance_Claim.csv")
 
@@ -50,8 +50,8 @@ podstaw2 <- function(x){
 dane_dobre <- podstaw2(dane_pop)
 cbind("CREDIT_SCORE" = any(dane_dobre$CREDIT_SCORE == TRUE),"ANNUAL_MILEAGE" = any(dane_dobre$ANNUAL_MILEAGE == TRUE))
 
-# Klasyfikacja za pomocą jednego modelu. ----------------------------------
-#Na początku projektu sprawdzę jak działa pojedynczy klasyfikator aby później porównać jego jakość z zespołowymi.
+# Classification using one model. ----------------------------------
+#At the beginning of the project, I will check how a single classifier works and then compare its quality with the team classifiers.
 
 dane_dobre$OUTCOME <- factor(dane_dobre$OUTCOME)
 levels(dane_dobre$OUTCOME) <- c("Dostanie","Nie dostanie")
@@ -64,18 +64,18 @@ fancyRpartPlot(model)
 
 predykcja <- predict(model, newdata = zbior_testowy, type = "class")
 confusionMatrix(predykcja, zbior_testowy$OUTCOME, mode = "everything", positive="Dostanie")
-#F-miara wynosi 0.8784 co oznacza, że model jest całkiem dobry.
+#The F-measure is 0.8784 which means that the model is quite good.
 
 
-# Klasyfikacja zespołowa - Las losowy -------------------------------------
+# Team classification - Random Forest -------------------------------------
 set.seed(456)
 las_losowy <- randomForest(OUTCOME~.,data=zbior_treningowy,ntree=1000)
 predykcja_las <- predict(las_losowy,newdata = zbior_testowy, type="class")
 confusionMatrix(predykcja_las, zbior_testowy$OUTCOME, mode = "everything", positive="Dostanie")
-#Wartość F-miary w przypadku lasu losowego wyszła 0.8914.
-#Możemy więc stwierdzić, że klasyfikator oparty na lesie losowym jest lepszy.
+#The F-measure value in the case of random forest was 0.8914.
+#So we can conclude that the random forest based classifier is better.
 
-#Wykres błędu średniokwadratowego OOB w lesie losowym.
+#OOB mean square error plot in random forest.
 las_losowy$err.rate
 plot(las_losowy)
 las_losowy.legenda <- colnames(las_losowy$err.rate)
@@ -86,20 +86,20 @@ min.err
 min.err.idx <- which(las_losowy$err.rate[,"OOB"]== min.err)
 min.err.idx
 las_losowy$err.rate[min.err.idx,]
-#Patrząc na wykres błędów, można stwierdzić, że błąd OOB jest dosyć wysoki lecz potem szybko spada i jest stabilny.
+#Looking at the error graph, it can be concluded that the OOB error is quite high, but then it drops quickly and is stable.
 
-# Uczenie ze wzmocnieniem -------------------------------------------------
+# Reinforcement learning -------------------------------------------------
 
-#Zastosuje model Adaboost.
+#Will use the Adaboost model.
 wzmocnienie <- boosting(OUTCOME~., zbior_treningowy,mfinal = 50)
 wzmocnienie$importance
 predykcja_ada <- predict(wzmocnienie,newdata = zbior_testowy, type="class")
 table(predykcja_ada$class,zbior_testowy$OUTCOME)
 confusionMatrix(as.factor(predykcja_ada$class), zbior_testowy$OUTCOME, mode = "everything", positive="Dostanie")
-#Wartość F-miary wyszła 0.8860. Jest ona mniejsza niż dla ucznia zespołowego opartego na baggingu bez wzmacniania.
+#The F-measure value was 0.8860. It is smaller than for a team learner based on bagging without reinforcement.
 
-#Metody gradientowe.
-#Ponowne przygotowanie danych i zamiana na zmienne numeryczne.
+#Gradient methods.
+#Re-preparing data and converting it to numerical variables.
 dane_dobre_dv <- dummyVars("~ .",dane_dobre[-19], fullRank = T)
 dane_dobre_dv_df <- as.data.frame(predict(dane_dobre_dv,newdata = dane_dobre[-19]))
 dane_dobre_dv_df <- cbind(dane_dobre_dv_df,dane_dobre[19])
@@ -126,9 +126,9 @@ coords(gbm.roc, "best")
 gradient_klasy <- ifelse(gradient_predykcje < 1.400101, "Dostanie", "Nie dostanie")
 confusionMatrix(as.factor(gradient_klasy), dane_dobre_dv_df_test$OUTCOME, mode = "everything", positive="Dostanie")
 table(gradient_klasy,dane_dobre_dv_df_test$OUTCOME)
-#F-miara wynosi: 0.8771.
+#F-measure is: 0.8771.
 
-#XGBM <- ulepszona wersja GBM
+#XGBM <- improved version of GBM
 levels(dane_dobre_dv_df_test$OUTCOME)<-c(1,0)
 dane.macierz <- as.matrix(dane_dobre_dv_df_train[,-26])
 trening.macierz <- as(dane.macierz,"dgCMatrix")
@@ -147,30 +147,30 @@ xgb_klasy <- ifelse(xgb.predykcja < 1.309854, "Dostanie", "Nie dostanie")
 levels(dane_dobre_dv_df_test$OUTCOME)<-c("Dostanie","Nie dostanie")
 confusionMatrix(as.factor(xgb_klasy), dane_dobre_dv_df_test$OUTCOME, mode = "everything", positive="Dostanie")
 table(xgb_klasy,dane_dobre_dv_df_test$OUTCOME)
-#F-miara wynosi: 0.8436.
-#W metodzie została wykorzystane do klasyfikacji regresja logistyczna, ustawiona domyślnie w funkcji xgboost.
+#F-measure is: 0.8436.
+#The method uses logistic regression for classification, set by default in the xgboost function.
 
-# Sieć neuronowa ----------------------------------------------------------
-#Zastosuję sieć neuronową do klasyfikacji użytych wcześniej danych. Tym razem wybiorę jedynie 4 zmienne.Ucznie sieci neuronowej wymaga najwięcej
-#operacji przez co czas pracy wydłużyłby się w porównaniu z innymi klasyfikatorami.
+# Neuron network ----------------------------------------------------------
+#I will use a neural network to classify the previously used data. This time I will choose only 4 variables. Neural network students require the most
+#operations, which would extend the working time compared to other classifiers.
 set.seed(456)
 levels(dane_dobre_dv_df_train$OUTCOME) <- c("Dostanie","Nie dostanie")
 model_sieci <- neuralnet(OUTCOME~VEHICLE_OWNERSHIP+PAST_ACCIDENTS+SPEEDING_VIOLATIONS+DUIS,data=dane_dobre_dv_df_train,hidden=2,lifesign="full", lifesign.step=5000)
 plot(model_sieci)
-#Ocena modelu
+#Model evaluation
 siec_predykcja <- neuralnet::compute(model_sieci,dane_dobre_dv_df_test[-26])
 str(siec_predykcja)
 najlepsza.kolumna <- apply(siec_predykcja$net.result,1,which.max)
 siec.predykcje = c("Dostanie","Nie dostanie")[najlepsza.kolumna]
-#Macierz pomyłek
+#Confusion matrix
 levels(dane_dobre_dv_df_test)<-c("Dostanie","Nie dostanie")
 table(dane_dobre_dv_df_test$OUTCOME,siec.predykcje)
 nrow(dane_dobre_dv_df_test)
 confusionMatrix(as.factor(siec.predykcje), dane_dobre_dv_df_test$OUTCOME, mode = "everything", positive="Dostanie")
-#F-miara wynosi: 0.8603.
+#F-measure is: 0.8603.
 
 
-#Wykonam jeszcze klasyfikację za pomocą algorytmu SVM tym razem na zbiorze danych voice.csv.
+#I will perform another classification using the SVM algorithm, this time on the voice.csv data set.
 set.seed(456)
 dane2 <- read.csv("voice.csv")
 dane2$label <- as.factor(dane2$label)
@@ -183,20 +183,20 @@ svm.predykcje <- predict(model_svm, voice_test)
 svm_tabelka <- table(svm.predykcje,voice_test$label)
 svm_tabelka
 confusionMatrix(svm.predykcje, voice_test$label, mode = "everything", positive="male")
-#F-miara wynosi: 0.8004.
+#F-measure is: 0.8004.
 
 
 # Podsumowanie ------------------------------------------------------------
 
-#Możemy stwierdzić, że zarówno pojedynczy klasyfikator oraz złożony osiągnęły podobne wyniki błędów dla zbioru danych Car_Insurance_Claim.
-#Średnio wynik klasyfikatorów wynosił ...
+#We can conclude that both the single classifier and the composite classifier achieved similar error scores for the Car_Insurance_Claim dataset.
+#On average, the classifiers' score was...
 wynik <- mean(0.8784,0.8914,0.8860,0.87717,0.8436)
 wynik
-#Średni wynik F-miary 0.8784
-#Sieć neuronowa na mniejszej ilości zmiennych ze zbioru Car_Insurance_Claim oraz SVM na danych voice.csv również osiągnęły wartości F-miary
-#powyżej 0.8.
-#Możemy zatem stwiedzić, że wykorzystane powyżej dane nadają się do klasyfikacji różnymi metodami.
-#Zobaczmy wydajność czasową sieci neuronowej oraz SVM.
+#Average F-measure score 0.8784
+#The neural network on a smaller number of variables from the Car_Insurance_Claim set and SVM on voice.csv data also achieved F-measure values
+#above 0.8.
+#We can therefore conclude that the data used above are suitable for classification using various methods.
+#Let's see the time efficiency of the neural network and SVM.
 siec_test <- function(x){
   siec_test_wynik <- neuralnet(OUTCOME~VEHICLE_OWNERSHIP+PAST_ACCIDENTS+SPEEDING_VIOLATIONS+DUIS,data=dane_dobre_dv_df_train,hidden=2,lifesign="full", lifesign.step=5000)
   return(siec_test_wynik)
